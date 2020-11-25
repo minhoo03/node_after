c1f8367dcbcdd0f6f86daf792c5b38fc67a09e56
minhoo03 <minhoo0333@gmail.com>
2020-11-25 19:49:51 +0900

    npm i express-session

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const session = require('express-session')
 const {User} = require('./models/User')
 require('./lib/mongoose')
 const crypto = require('crypto')
@@ -9,25 +10,28 @@ const port = 3001;
 // app.use('/public',express.static("./public"));
 app.use(express.json())
 app.use(express.urlencoded())
-// app.use((req, res, next) => {
-//   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
-//   console.log(`[${req.method}] ${req.path} ${ip}`)
-//   next()
-// })
 app.use(express.static("./public"));
+app.set('view engine', 'ejs')
+app.use(session({
+  secret: '($YA)@#12asd^%#',
+  resave: false,
+  saveUninitialized: true
+}))
 
-let session = false
+app.get('/', (req, res) => {
+  res.render('main', { user: { name: '김민후' } })
+})
+
+app.get('/registry', (req, res) => {
+  res.render('registry')
+})
 
 app.post('/login', async (req, res) => {
   const { body: { id, pw }} = req
   console.log(req.user)
-  if(session) {return res.send(`${session}, 이미 로그인`);}
   const epw = crypto.createHash('sha512').update(id+'lib'+pw+'salt').digest('base64')
   const data = await User.find({ id, pw: epw })
-  if(data.length) session = true
   res.json(data);
-
-  // res.send(`login failed ${id} ${pw}`)
 })
 
 app.post('/registry', (req, res) => {
@@ -39,9 +43,7 @@ app.post('/registry', (req, res) => {
 })
 
 app.get('/logout', (req, res) => {
-  session = false
   res.redirect('/')
-  console.log(`logout && ${session}`)
 })
 
 app.get('/users', async (req, res) => { // find{조건, }
